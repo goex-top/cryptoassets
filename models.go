225fba487a3c670eca79e19638a484c273e3f084
eric <btstarinfo@gmail.com>
2020-02-27 00:44:38 +0800

    update db models

--- models.go
+++ models.go
@@ -7,24 +7,29 @@ import (
 // database table
 type Account struct {
 	gorm.Model
-	NickName      string `gorm:"unique"`
-	ExchangeName  string
-	ApiKey        string
-	ApiSecretKey  string
-	ApiPassphrase string
-	Assets        []Asset
+	NickName       string `gorm:"unique"`
+	ExchangeName   string
+	ApiKey         string
+	ApiSecretKey   string
+	ApiPassphrase  string
+	Assets         []Asset
+	LastUpdateTime int64
 }
 
 type Asset struct {
 	gorm.Model
-	Btc      float64
-	Usdt     float64
-	Usd      float64
-	Cny      float64
-	Btc_Usdt float64
-	Usdt_Usd float64
-	Usd_Cny  float64
-	Coins    []CoinAsset
+	AccountID uint
+	Btc       float64
+	Usdt      float64
+	Usd       float64
+	Cny       float64
+	Btc_Usdt  float64
+	Btc_Usd   float64
+	Btc_Cny   float64
+	Usdt_Usd  float64
+	Usdt_Cny  float64
+	Usd_Cny   float64
+	Coins     []CoinAsset
 }
 
 type CoinAsset struct {
